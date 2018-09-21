75093f3d76ca284af66df16c7bd23f70a8439553
David Jung <djung31@gmail.com>
2018-09-21 10:13:41 -0500

    set up middleware and started app

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
+
+const PORT = 1337;
+
+
+app.get("/", (req, res) => {
+  res.send("<h1>HELLO WORLD</h1>");
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
+
