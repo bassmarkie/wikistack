742cd7dcf793fa5307ad0e7eb5ea7fcbab4a61e7
David Jung <djung31@gmail.com>
2018-09-21 11:00:22 -0500

    database starting

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const views = require('./views/index')
+const { db } = require('./models');
+// const { Page, User } = require('./models/index');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
+
 
 const PORT = 3000;
 
@@ -17,7 +20,20 @@ app.get("/", (req, res) => {
   res.send(views.main(post));
 })
 
-app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
-});
+const sync = async () => {
+  await db.Page.sync();
+  await db.User.sync();
+  app.listen(PORT, () => {
+    console.log(`App listening in port ${PORT}`);
+  });
+}
+sync();
 
+// app.listen(PORT, () => {
+//   console.log(`App listening in port ${PORT}`);
+// });
+
+// db.authenticate().
+// then(() => {
+//   console.log('connected to the database');
+// });
